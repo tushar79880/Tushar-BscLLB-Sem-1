988b205c372ed12001a24290fdd8c82616fae59c
tushar79880 <158560016+tushar79880@users.noreply.github.com>
2025-08-28 11:35:33 +0530

    Add files via upload

---
+++ SUMOF2NUMCODE.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main ()
+{
+    int a,b,sum;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    printf("enter the value of b");
+    scanf("%d",&b);
+    sum = a+b;
+    printf("Sum: %d\n", sum);
+
+
+}
